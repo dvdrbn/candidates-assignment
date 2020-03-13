f51cb3f2f79de5bb884eaefb419c74fe955344f6
David <david.rvb@gmail.com>
2020-03-13 09:40:11 -0700

    Added all exercises & contact details

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+# set target server by local hostname
+if [ "$(hostname)" = "server1" ]; then 
+	target_server="server2"
+else
+	target_server="server1"
+fi
+
+# last argument is target directory
+target_dir=${@: -1}
+
+# transfer files via ssh
+scp -v ${@:1:$#-1} $target_server:$target_dir |& grep Sink |& awk '{sum += $3} END {print sum}'
+
+# ${@:1:$#-1} - will give all argument except last
+# grep Sink   - will catch lines containing bytes of every file
+# awk '{sum += $3} END {print sum}' - will sum the bytes catched by grep
